cb50bf6c529f1063fa74f7862263ee64c25de8dc
Fredrik H. M. Saether <frerikhms@gmail.com>
2025-08-14 00:04:35 +0200

    cleanup agent and fork spesific changes

--- cleanup/forcedeleteolm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-BAD_NAMESPACE=olm
-kubectl get namespace "$BAD_NAMESPACE" -o json   | tr -d "\n" | sed "s/\"finalizers\": \[[^]]\+\]/\"finalizers\": []/"   | kubectl replace --raw /api/v1/namespaces/$BAD_NAMESPACE/finalize -f -
